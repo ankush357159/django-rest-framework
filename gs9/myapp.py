aec4bb30102e9259e1d7f3b424fafcea8bd5d13c
Ankush <ankush357159@gmail.com>
2021-12-04 16:42:57 +0530

    api_view

---
+++ gs9/myapp.py
@@ -0,0 +1,58 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    headers = {'content-Type': 'application/json'}
+    r = requests.get(url=URL, headers=headers, data=json_data)
+    data = r.json()
+    print(data)
+
+# get_data(3)
+# get_data()
+
+def post_data():
+    data = {
+        'name': 'Pappu',
+        'roll': 100,
+        'city': 'Panvel'
+    }
+
+    json_data = json.dumps(data)
+    headers = {'content-Type': 'application/json'}
+    r = requests.post(url = URL, headers=headers, data = json_data)
+    data = r.json()
+    print(data)
+
+post_data()
+
+def update_data():
+    data = {
+        'id': 3,
+        'name': 'Sawant',
+        'roll': 103,        
+    }
+
+    json_data = json.dumps(data)
+    r = requests.put(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+# update_data()
+
+def delete_data():
+    data = {
+        'id': 3              
+    }
+
+    json_data = json.dumps(data)
+    r = requests.delete(url = URL, data = json_data)
+    data = r.json()
+    print(data)
+
+# delete_data()
