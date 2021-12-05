95a4e166807acf4d6d046ea030763e8b894dee93
Ankush <ankush357159@gmail.com>
2021-12-05 19:58:24 +0530

    authentication setting

---
+++ gs20/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import authentication
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    # permission_classes = [AllowAny]
+    # permission_classes = [IsAdminUser]
+   
