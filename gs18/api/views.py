b82c0bce2b1879e41fb2511ecaddea0fcfbfa271
Ankush <ankush357159@gmail.com>
2021-12-05 13:21:02 +0530

    modelviewset

---
+++ gs18/api/views.py
@@ -0,0 +1,8 @@
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework import viewsets
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    print(queryset)
