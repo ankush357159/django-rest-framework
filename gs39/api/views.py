c17947cccc308e7b50ec8b26f0adbc606ee63f93
Ankush <ankush357159@gmail.com>
2021-12-07 15:58:41 +0530

    Search, pagination

---
+++ gs39/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from .myPagination import MyOffsetPagination
+from .serializers import StudentSerializer
+from .models import Student
+from rest_framework.generics import ListAPIView
+
+# Create your views here.
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    pagination_class = MyOffsetPagination
+
